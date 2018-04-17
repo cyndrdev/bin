ded4c9f9e7f482282f97389b33c0ff5c038a2a4a
cyndrdev <me@cyndr.uk>
2018-04-17 15:55:03 +0100

    added discord background syncer

---
+++ discordbg/update.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# sript by cyndrdev
+# depends on betterdiscord, wal, xrandr and imagemagick
+
+# discord theme by WhatTDaPuck
+
+bg=$(cat "$HOME/.cache/wal/wal")
+bg_tmp=$(mktemp tmp.XXXXXXXX.jpg -p /tmp)
+theme="wal.theme.css"
+
+function get_screen_dims(){
+	xrandr | grep '*' | grep -Eo '^\s.*\s' | tr -d '[:space:]'
+}
+
+function convert_bg(){
+	convert $bg -resize "$(get_screen_dims)^" $bg_tmp
+	convert $bg_tmp -gravity center -crop $(get_screen_dims)+0+0 $bg_tmp
+}
+
+function reload_discord(){
+	#kill -USR1 $(pgrep Discord | tail -n 1)
+	sleep 0
+}
+
+if [ -f "$bg" ]; then
+	convert_bg
+	data=$(base64 --wrap=0 $bg_tmp)
+
+	cd "$(dirname $0)"
+
+	cat head > $theme
+	echo "    background: url(data:image/jpg;base64,$data) !important;" >> $theme
+	cat tail >> $theme
+	rm $bg_tmp
+	reload_discord
+fi
